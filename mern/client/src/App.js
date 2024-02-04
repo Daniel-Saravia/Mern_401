f0e0ba8aef1b4ca34e8d5b428c20631d55cc0107
Big Boss <saraviad4444@gmail.com>
2024-02-04 09:31:56 -0700

    fixed login not showing

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { AuthProvider, useAuth } from "./context/AuthContext"; // Import AuthProvider and useAuth
+import { Route, Routes, Navigate } from "react-router-dom";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
-import Login from "./components/Login"; // Make sure you have this component
+import Login from "./components/Login";
 
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
 function PrivateRoute({ children }) {
   const { currentUser } = useAuth();
   return currentUser ? children : <Navigate to="/login" />;
@@ -16,42 +14,40 @@ function PrivateRoute({ children }) {
 
 const App = () => {
   return (
-    <Router>
-      <AuthProvider>
-        <div>
-          <Navbar />
-          <div style={{ margin: 20 }}>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route
-                path="/"
-                element={
-                  <PrivateRoute>
-                    <RecordList />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/edit/:id"
-                element={
-                  <PrivateRoute>
-                    <Edit />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/create"
-                element={
-                  <PrivateRoute>
-                    <Create />
-                  </PrivateRoute>
-                }
-              />
-            </Routes>
-          </div>
+    <AuthProvider>
+      <div>
+        <Navbar />
+        <div style={{ margin: 20 }}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <RecordList />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/edit/:id"
+              element={
+                <PrivateRoute>
+                  <Edit />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/create"
+              element={
+                <PrivateRoute>
+                  <Create />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
         </div>
-      </AuthProvider>
-    </Router>
+      </div>
+    </AuthProvider>
   );
 };
 
